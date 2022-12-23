5e6e8f822765fa800e22747c541e7c88cd2705e8
Mirza Maulana <96324241+MirzaMaulana@users.noreply.github.com>
2022-12-23 17:13:03 +0700

    Add files via upload

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,7 @@
 // Year at the bottom
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+let loading = document.getElementById("loading");
+window.addEventListener("load", function () {
+  loading.style.display = "none";
+});
